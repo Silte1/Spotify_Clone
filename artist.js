0952e18a8dfb78da8cf4ac8c2d75858ed2539efd
micha-lieber <michael.lieber@dci-student.de>
2023-01-11 15:34:03 +0100

    added autocomplete with tracks

--- artist.js
+++ artist.js
@@ -5,9 +5,11 @@ const artistTop = document.getElementById("artist-top");
 const monthlyListeners = document.getElementById("monthly-listeners");
 const artistName = document.getElementById("artist-name");
 
-const artistbottom = document.getElementById("artist-bottom");
+const artistBottom = document.getElementById("artist-bottom");
+const artistAlbumsContent = document.getElementById("artist-albums-content");
 
 async function getAlbums(artist) {
+  console.log(artist);
   const options = {
     method: "GET",
     headers: {
@@ -17,14 +19,33 @@ async function getAlbums(artist) {
   };
 
   const response = await fetch(
-    `https://deezerdevs-deezer.p.rapidapi.com/search?q=artist:'${artist}'`,
+    `https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`,
     options
   );
   const data = await response.json();
   console.log(data);
   // von /artist/id kriegen wir mit data.name den Namen
-  // artistName.innerText = data.name;
-  // artistTop.style.background = `url(${data.picture_xl})`;
+  artistName.innerText = data.data[0].artist.name;
+  artistTop.style.background = `url(${data.data[0].artist.picture_xl})`;
+
+  data.data.map((track) => {
+    const card = document.createElement("div");
+    const image = document.createElement("img");
+    const title = document.createElement("h5");
+    const artistName = document.createElement("p");
+
+    card.classList.add("card");
+    image.classList.add("card-img-top");
+    title.classList.add("card-title", "text-center", "text-light");
+    artistName.classList.add("card-text", "text-center", "text-light");
+    console.log(track.album.cover);
+    image.src = track.album.cover;
+    title.innerText = track.title;
+    artistName.innerText = track.artist.name;
+
+    card.append(image, title, artistName);
+    artistAlbumsContent.append(card);
+  });
 }
 
 artistPlay.addEventListener("click", (e) => {
