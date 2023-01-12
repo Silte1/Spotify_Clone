300d05683db36205c68fec74d3e15e94c80b15cd
Silte1 <etiennesilue3@gmail.com>
2023-01-12 16:02:57 +0100

    working on js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const searchBtn = document.querySelector('#addon-wrapping') ;
-const searchInput = document.querySelector('#searchInput').value,
-artistSection = document.querySelector('artist'),
-searchResult = document.querySelector('searchResult'),
-tractSection = document.querySelector('tract');
+const searchInput = document.querySelector('#searchInput'),
+artist = document.querySelector('#artist'),
+searchResult = document.querySelector('#searchResult'),
+tractSection = document.querySelector('#tract');
+
+// console.log(searchInput.textContent);
+// console.log(searchBtn.textContent);
+// console.log(artist.textContent);
+// console.log(searchResult.textContent);
+// console.log(tractSection.textContent);
+
 
 
 
@@ -19,9 +26,31 @@ const options = {
 
 const fetchingFunction = async () => {
   try {  
-    const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search`, options);
-    const data = await response.json()
+    let artistToSearch = (searchInput.value).toLowerCase();
+    const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistToSearch}`, options);
+    const data = await response.json();
+    
+    console.log(data.data);
     console.log(data);
+    data.data.map(elm => {
+      searchResult.innerHTML = `Search results for "${searchInput.value}"`;
+  
+      artist.innerHTML = `<h2>Artist</h2>
+      <div class="card" style="width: 18rem;">
+      <img src="${elm.artist.picture_medium}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h3 id = "artistName">${(searchInput.value)}</h3>
+        <p id="card-text">artist</p>
+      </div>
+    </div>`
+    document.querySelector('#artistName').style.color = 'black';
+    document.querySelector('#card-text').style.color = 'black';
+
+
+
+    })
+    
+
     if(!response.ok){
       throw new Error(`Error: ${response.status}` )
     }
@@ -37,6 +66,8 @@ const fetchingFunction = async () => {
 
 
 searchBtn.addEventListener('click', (e)=>{
-    e.preventDefault()
-    fetchingFunction()
+  console.log("hier",searchInput.value)
+
+    
+     fetchingFunction()
   }) 
