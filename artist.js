b44e27f72ff4721f684bc0045bb909533a8165fe
micha-lieber <michael.lieber@dci-student.de>
2023-01-13 09:34:15 +0100

    added comments in code

--- artist.js
+++ artist.js
@@ -1,5 +1,7 @@
 import { API_KEY, API_URL } from "./env.js";
 
+//::::::::::::::::: Variables
+
 const artistPlay = document.getElementById("artist-play");
 const artistTop = document.getElementById("artist-top");
 const artistFollow = document.getElementById("artist-follow");
@@ -18,10 +20,13 @@ const options = {
     "X-RapidAPI-Host": API_URL,
   },
 };
+
 // use the artist variable to change the artists page
 const artist = "ksbloom";
 
-// creates a message that artist has been added to list. STILL NEEDS TO ADD ARTIST TO SAID LIST
+//::::::::::::::::: Functions
+
+/** creates a message that artist has been added to "follow" list. STILL NEEDS TO ADD ARTIST TO SAID LIST */
 function followArtist() {
   artistAddMessage.style.display = "block";
   setTimeout(() => {
@@ -32,7 +37,10 @@ function followArtist() {
 /**function fills the artist page dynamically with content */
 async function getAlbums(artist) {
   try {
+    // data needs to be defined before the do while loop further down in the code. if we don't define it here, the loop does not work
+
     let data;
+
     // resets the artist bottom page
     document.getElementById("artist-albums").innerText = "Albums";
     document.getElementById("artist-albums-content").innerText = "";
@@ -46,10 +54,11 @@ async function getAlbums(artist) {
       data = await response.json();
     } while (!data.data);
 
-    // von /artist/id kriegen wir mit data.name den Namen
-    artistName.innerText = artist.toUpperCase(); //data.data[0].artist.name;
+    // enters artist Name and picture in the artist top page
+    artistName.innerText = artist.toUpperCase();
     artistTop.style.background = `url(${data.data[0].artist.picture_xl})`;
 
+    // loops over tracks of the artist and displays their information in the "albums-content" element
     data.data.map((track) => {
       const card = document.createElement("div");
       const image = document.createElement("img");
@@ -69,15 +78,15 @@ async function getAlbums(artist) {
       artistAlbumsContent.append(card);
     });
 
-    // plays the preview of one of the first 25 songs when clicking on "PLAY"
+    // Eventlistener on "play" : plays the preview of one of the first 25 songs
     artistPlay.addEventListener("click", (e) => {
       let random = Math.floor(Math.random() * 25);
       const a = new Audio(data.data[random].preview);
       a.play();
     });
-    // Eventlistener on "follow"
+
+    // Eventlistener on "follow", adds artist to follow list
     artistFollow.addEventListener("click", (e) => {
-      console.log("added Artist to your playlist");
       followArtist();
     });
   } catch (error) {
@@ -85,11 +94,16 @@ async function getAlbums(artist) {
   }
 }
 
+//::::::::::::::: Events
+
+// Eventlistener "about": loads about artist page
 artistAbout.addEventListener("click", (e) => {
   document.getElementById("artist-albums").innerText = "About";
   document.getElementById("artist-albums-content").innerText =
     "There is really not much to say about them.";
 });
+
+// Eventlistener "related artists": loads related artists page
 
 artistRelatedArtists.addEventListener("click", (e) => {
   document.getElementById("artist-albums").innerText = "Related Artists";
@@ -97,37 +111,31 @@ artistRelatedArtists.addEventListener("click", (e) => {
     "Nobody makes music the way they do!";
 });
 
+// Eventlistener "overview": loads album page
 artistOverview.addEventListener("click", (e) => {
   getAlbums(artist);
 });
-//fills the page with content
-getAlbums(artist);
+
+// :::::::::::::: H O M E P A G E Functions
+
+//::::::::::::  Variables
+// swiper for home page
+const swiperWrapper = document.getElementsByClassName("swiper-wrapper")[0];
 
 // Album Ids for Home page
 const throwbackAlbumIds = [
   340077257, 352669977, 384131297, 6019334, 362299477, 230638702, 95185612,
   150710522, 15559256, 221439032, 108934,
 ];
-const topFourtyId = [9574940];
+const topFortyId = [9574940];
 
-// fill swiper with albums
-const swiperWrapper = document.getElementsByClassName("swiper-wrapper")[0];
-
+/** Function to fetch informations on an album */
 async function fillSwiper(param) {
   try {
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": API_URL,
-      },
-    };
-
     const response = await fetch(
       `https://deezerdevs-deezer.p.rapidapi.com/album/${param}`,
       options
     );
-
     const data = await response.json();
     return data;
   } catch (error) {
@@ -135,19 +143,24 @@ async function fillSwiper(param) {
   }
 }
 
+// this loop fills the "throwback" Element on homepage with albums
 throwbackAlbumIds.forEach(async (element) => {
   try {
+    // data needs to be defined before the do while loop or else the loop does not work
     let data;
-    let n = 0;
+
+    // repeats fetching, until data.title is NOT undefined
     do {
-      n++;
       data = await fillSwiper(element);
-      console.log(n);
     } while (!data.title);
+
+    //creates the HTML Elements for the album
     const card = document.createElement("div");
     const image = document.createElement("img");
     const title = document.createElement("p");
     card.classList.add("swiper-slide");
+
+    // fills the HTML elements with content for the album
     image.src = `${data.cover_medium}`;
     title.innerText = data.title.toUpperCase();
 
@@ -158,7 +171,7 @@ throwbackAlbumIds.forEach(async (element) => {
   }
 });
 
-////////////////////swiper js begins
+//:::::::::::::::: S W I P E R for home page
 
 const swiper = new Swiper(".swiper", {
   // Optional parameters
@@ -172,9 +185,7 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
 });
+
+//fills the page with content
+getAlbums(artist);
