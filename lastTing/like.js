30d6d06b743a85ec2173264c4f908f44de83a8d1
Aliramazanian <ali.ramazanian1971@gmail.com>
2023-01-16 11:08:25 +0100

    done

--- lastTing/like.js
+++ lastTing/like.js
@@ -1,6 +1,6 @@
 $(".love").click(function () {
  $('.heart').toggleClass('love');
- $('.line, .heart').addClass("active").delay(300).queue(function (next) {
+ $('.line, .heart').addClass("active").delay(100).queue(function (next) {
   $('.line, .heart').removeClass("active");
   next();
  });
